c18d1b264f50e30f471af86949ab8875ffbe863e
Shailendra Pratap Singh <shailendraapi06@gmail.com>
2026-09-15 23:19:07 +0530

    products by category

--- store/views.py
+++ store/views.py
@@ -1,14 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
+from category.models import Category
 
 
-def store(request):
-	products = Product.objects.filter(is_available=True)
+def store(request, category_slug=None):
+    category = None
+    products = None
 
-	product_count = products.count()
+    if category_slug is not None:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=categories, is_available=True)
+        product_count = products.count()
+    else:
+        products = Product.objects.all().filter(is_available=True)
+        product_count = products.count()
 
-	context = {
-		'products': products,
-		'product_count': product_count
-	}
-	return render(request, 'store/store.html', context)
+    context = {
+        'products': products,
+        'product_count': product_count
+    }
+    return render(request, 'store/store.html', context)
