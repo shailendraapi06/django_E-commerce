c5c796e1cf8ed08d6799daf69ba10a12355fc78b
Shailendra Pratap Singh <shailendraapi06@gmail.com>
2026-09-17 01:18:18 +0530

    add to cart logic

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Cart, CartItem
+from store.models import Product
 
 # Create your views here.
+from django.http import HttpResponse
+
+def _cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+
+def add_cart(request, product_id):
+    product = Product.objects.get(id=product_id) # get the product 
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request)) # get the cart using the cart_id present in the session
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(
+            cart_id = _cart_id(request)
+        )
+        cart.save()
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item.quantity += 1 # increase the quantity of the cart item
+        cart_item.save()
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(
+            product=product,
+            cart=cart,
+            quantity=1
+        )
+        cart_item.save()
+        return HttpResponse(cart_item.product) # return a response to the user
+
+    return redirect('cart') # redirect to the cart page
 
 def cart(request):
     return render(request, 'store/cart.html')
